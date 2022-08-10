9d8170406ae812f16c54ea341c3eb1be688a79ea
Luca Fiorio <luca.fiorio@outlook.com>
2022-08-10 16:19:56 +0200

    Add functionalities for WINGST BOM.

--- scripts/prepare_BOM_rename_files.m
+++ scripts/prepare_BOM_rename_files.m
@@ -6,80 +6,130 @@ clc
 
 global numberOfLinesInTheExcelBOM
 global excelBOMRangeWhereAddLines
-global excelInputTableName
-global excelOutputTableName
 global documentationOutputFolderName
-global documentationInputFolderName
+global documentationInputFolderPath
+global ExcelOutputFileStartingLine
 
 numberOfLinesInTheExcelBOM = 16;
 excelBOMRangeWhereAddLines = 'A25:L25';
-excelInputTableName = 'Book1.xlsx';
-excelOutputTableName = 'BOM.xlsx';
 documentationOutputFolderName = 'documentation';
-documentationInputFolderName = 'data';
-
 ExcelOutputFileStartingLine = 10;
 
+caseBomFromWingst = 1;
+caseBomFromCreo = 2;
 
 %% Main
+msg = "Select what you want to do";
+opts = ["I have a BOM from WINGST and I want to rename the production files" "I have a simplified BOM from CREO, I want to rename files and prepare the production BOM"];
+choice = menu(msg,opts);
 
-table = readcell(excelInputTableName);
-quantityCOlumnIsCorrect = all(table{1,2} == 'QUANTITY');
-
-if ~quantityCOlumnIsCorrect
-    error('The Excel table is not compliant. Check the QUANTITY column.')
+switch choice
+   case caseBomFromWingst
+      prepareDocumentationForBomFromWingst()
+   case caseBomFromCreo
+      prepareDocumentationForBomFromCreoAndFillProductionBom()
+   otherwise
+      error('No correct selection has been provided.')
 end
 
 
-numberOfItems = length(table)-1;
+%% BOM from WINGST section
 
-if numberOfItems > numberOfLinesInTheExcelBOM
-    addLines(numberOfItems - numberOfLinesInTheExcelBOM)
-end
+function prepareDocumentationForBomFromWingst()
 
-mkdir(documentationOutputFolderName)
-
-for i = 1 : numberOfItems
-    quantity = table{1+i, 2};
-    aliasAsCell = table(1+i, 3);
-    aliasAsString = table{1+i, 3};
-    revisionAsCell = table(1+i, 4);
-    revisionAsString = table{1+i, 4};
+    global documentationOutputFolderName
+    global documentationInputFolderPath
+    global ExcelOutputFileStartingLine
     
-    % Convert revision to string if it is 0
-    if revisionAsString == 0 
-        revisionAsString = num2str(0);
+    aliasColumnInInputTable = 3;
+    revisionColumnInInputTable = 4;
+    
+    [excelInputTableName,pathInputTableName] = uigetfile({'*.xlsx';'*.xlsx'},'Select input BOM table from WINGST. In the same folder there MUST be the pdf and step files.');
+    
+    inputTable = readcell(join([pathInputTableName, excelInputTableName]));
+    
+    maxTableLength = length(inputTable);
+    
+    for i = ExcelOutputFileStartingLine : maxTableLength
+        cellIsEmpty = any(ismissing(inputTable{i, 1}));
+        if cellIsEmpty
+            break
+        end
     end
     
-    xlswrite(excelOutputTableName, quantity, 'Sheet1', join(['H', num2str(i+ExcelOutputFileStartingLine)]))
-    xlswrite(excelOutputTableName, aliasAsCell, 'Sheet1', join(['D', num2str(i+ExcelOutputFileStartingLine)]))
-    xlswrite(excelOutputTableName, revisionAsCell, 'Sheet1', join(['E', num2str(i+ExcelOutputFileStartingLine)]))
+    numberOfItems = inputTable{i-1, 1};
     
-    sourceDrawingFileName = join([aliasAsString, '.pdf']);
-    sourceStepFileName = join([aliasAsString, '.stp']);
+    documentationInputFolderPath = pathInputTableName;
+    mkdir(join([documentationInputFolderPath, documentationOutputFolderName]));
     
-    outputDrawingFileName = join([aliasAsString, '_rev_', revisionAsString, '.pdf']);
-    outputStepFileName = join([aliasAsString, '_rev_', revisionAsString, '.stp']);
-    % Remove spaces (might be necessary)
-    outputDrawingFileName = outputDrawingFileName(find(~isspace(outputDrawingFileName)));
-    outputStepFileName = outputStepFileName(find(~isspace(outputStepFileName)));
-    
-    copyAndRenameFile (sourceDrawingFileName, outputDrawingFileName)
-    copyAndRenameFile (sourceStepFileName, outputStepFileName)
+    for j = ExcelOutputFileStartingLine : ExcelOutputFileStartingLine + numberOfItems
+        aliasAsString = inputTable{j, aliasColumnInInputTable};
+        revisionAsString = inputTable{j, revisionColumnInInputTable};
+        revisionAsString = checkZeroRevision(revisionAsString);
+        
+        copyAndRenameProductionFiles(aliasAsString, revisionAsString)
+    end
+end
 
+
+%% BOM from Creo section
+
+function prepareDocumentationForBomFromCreoAndFillProductionBom()
+
+    global numberOfLinesInTheExcelBOM
+    global documentationInputFolderPath
+    global documentationOutputFolderName
+    global ExcelOutputFileStartingLine
+    
+    [excelInputTableName,pathInputTableName] = uigetfile({'*.xlsx';'*.xlsx'},'Select simplified input BOM table from Creo. In the same folder there MUST be the pdf and step files.');
+    [excelOutputTableName,pathOutputTable] = uigetfile({'*.xlsx';'*.xlsx'},'Select output BOM table for production');
+
+    excelOutputTableFileWithPath = join([pathOutputTable, excelOutputTableName]);
+
+    documentationInputFolderPath = pathInputTableName;
+    mkdir(join([documentationInputFolderPath, documentationOutputFolderName]));
+
+    table = readcell(join([pathInputTableName, excelInputTableName]));
+    quantityColumnIsCorrect = all(table{1,2} == 'QUANTITY');
+
+    if ~quantityColumnIsCorrect
+        error('The Excel table is not compliant. Check the QUANTITY column.')
+    end
+
+    numberOfItems = length(table)-1;
+
+    if numberOfItems > numberOfLinesInTheExcelBOM
+        numberOfLinesToAdd = numberOfItems - numberOfLinesInTheExcelBOM;
+        addLines(excelOutputTableFileWithPath, numberOfLinesToAdd)
+    end
+
+    for i = 1 : numberOfItems
+        quantity = table{1+i, 2};
+        aliasAsCell = table(1+i, 3);
+        aliasAsString = table{1+i, 3};
+        revisionAsCell = table(1+i, 4);
+        revisionAsString = table{1+i, 4};
+        
+        revisionAsString = checkZeroRevision(revisionAsString);
+
+        xlswrite(excelOutputTableFileWithPath, quantity, 'Sheet1', join(['H', num2str(i+ExcelOutputFileStartingLine)]))
+        xlswrite(excelOutputTableFileWithPath, aliasAsCell, 'Sheet1', join(['D', num2str(i+ExcelOutputFileStartingLine)]))
+        xlswrite(excelOutputTableFileWithPath, revisionAsCell, 'Sheet1', join(['E', num2str(i+ExcelOutputFileStartingLine)]))
+
+        copyAndRenameProductionFiles(aliasAsString, revisionAsString)
+
+    end
 end
 
 
 %% Functions
 
-function addLines(numberOfLinesToAdd)
+function addLines(excelOutputTableFileWithPath, numberOfLinesToAdd)
 
     global excelBOMRangeWhereAddLines
-    global excelOutputTableName
 
-    excelTableFile = join([pwd, '\', excelOutputTableName]);
     excel = actxserver('Excel.Application');
-    wb = excel.Workbooks.Open(excelTableFile);
+    wb = excel.Workbooks.Open(excelOutputTableFileWithPath);
     ws = wb.Worksheets.Item('Sheet1');
     
     for i = 1 : numberOfLinesToAdd
@@ -93,14 +143,39 @@ function addLines(numberOfLinesToAdd)
 end
 
 
+function revisionChecked = checkZeroRevision(revision)
+    % Convert revision to string if it is 0
+    if revision == 0 
+        revisionChecked = num2str(0);
+    else
+        revisionChecked = revision;
+    end
+end
+
+
+function copyAndRenameProductionFiles(aliasAsString, revisionAsString)
+        sourceDrawingFileName = join([aliasAsString, '.pdf']);
+        sourceStepFileName = join([aliasAsString, '.stp']);
+
+        outputDrawingFileName = join([aliasAsString, '_rev_', revisionAsString, '.pdf']);
+        outputStepFileName = join([aliasAsString, '_rev_', revisionAsString, '.stp']);
+        % Remove spaces (might be necessary)
+        outputDrawingFileName = outputDrawingFileName(find(~isspace(outputDrawingFileName)));
+        outputStepFileName = outputStepFileName(find(~isspace(outputStepFileName)));
+
+        copyAndRenameFile (sourceDrawingFileName, outputDrawingFileName)
+        copyAndRenameFile (sourceStepFileName, outputStepFileName)
+end
+
+
 function copyAndRenameFile (originalFileName, modifiedFileName)
 
     global documentationOutputFolderName
-    global documentationInputFolderName
+    global documentationInputFolderPath
 
-    originalFileNameWithDirectory = join([pwd, '\', documentationInputFolderName, '\', originalFileName]);
+    originalFileNameWithDirectory = join([documentationInputFolderPath, originalFileName]);
     if isfile(originalFileNameWithDirectory)
-        outputFileNameWithDirectory = join([pwd, '\', documentationOutputFolderName, '\', modifiedFileName]);
+        outputFileNameWithDirectory = join([documentationInputFolderPath, documentationOutputFolderName, '\', modifiedFileName]);
         copyfile(originalFileNameWithDirectory, outputFileNameWithDirectory);
     end
 end
