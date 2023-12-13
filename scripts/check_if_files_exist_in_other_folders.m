d53598bd3506ed4b83d173aaf71deb4d1e548416
Luca Fiorio <luca.fiorio@iit.it>
2023-12-13 15:14:35 +0100

    Update script to save a report also with files " not found" and better print in command line.

--- scripts/check_if_files_exist_in_other_folders.m
+++ scripts/check_if_files_exist_in_other_folders.m
@@ -31,8 +31,9 @@ end
 % Get a list of all files in Folder 1
 filesFolder1 = dir(fullfile(folder1, '*.*'));
 
-% Initialize a report
-report = cell(0);
+% Initialize reports for files found and not found
+reportFound = cell(0);
+reportNotFound = cell(0);
 
 % Check each file in Folder 1
 for i = 1:length(filesFolder1)
@@ -44,21 +45,34 @@ for i = 1:length(filesFolder1)
     % Check if the file exists in Folder 2 or its subfolders
     [fileExists, filePathFolder2] = checkFileExists(fullfile(folder2, '**', filename));
     
-    % Include in the report only if the file is found in Folder 2
+    % Include in the corresponding report section
     if fileExists
-        report{end+1} = sprintf('File: %s, Exists in: %s', filePathFolder1, filePathFolder2);
+        reportFound{end+1} = sprintf('File: %s, Exists in: %s', filePathFolder1, filePathFolder2);
+    else
+        reportNotFound{end+1} = sprintf('File: %s, Not found in: %s', filePathFolder1, folder2);
     end
 end
 
-% Display the report in the command window
-if ~isempty(report)
-    disp('Comparison Report (Files Found in Folder 2):');
-    disp(report);
-    % Save the report to a text file
-    saveReportToFile(report, 'Comparison_Report.txt');
-else
-    disp('No matching files found in Folder 2.');
+% Display the reports in the command window
+% Display the reports in the command window
+if ~isempty(reportFound)
+    disp('Files Found in Folder 2:');
+    for i = 1:numel(reportFound)
+        fprintf('%s\n', reportFound{i});
+    end
 end
+
+if ~isempty(reportNotFound)
+    disp(' ');
+    disp('Files Not Found in Folder 2:');
+    for i = 1:numel(reportNotFound)
+        fprintf('%s\n', reportNotFound{i});
+    end
+end
+
+% Save the reports to text files
+saveReportToFile(reportFound, 'Found_Report.txt');
+saveReportToFile(reportNotFound, 'NotFound_Report.txt');
 
 % Function to check if a file exists in a directory or its subdirectories
 function [exists, filePath] = checkFileExists(filePath)
@@ -72,17 +86,19 @@ function [exists, filePath] = checkFileExists(filePath)
     end
 end
 
-% Function to save the report to a text file
+% Function to save a report to a text file
 function saveReportToFile(report, filename)
-    fid = fopen(filename, 'w');
-    if fid == -1
-        error('Unable to create the report file.');
+    if ~isempty(report)
+        fid = fopen(filename, 'w');
+        if fid == -1
+            error('Unable to create the report file.');
+        end
+
+        for i = 1:length(report)
+            fprintf(fid, '%s\n', report{i});
+        end
+
+        fclose(fid);
+        disp(['Report saved to ' filename]);
     end
-    
-    for i = 1:length(report)
-        fprintf(fid, '%s\n', report{i});
-    end
-    
-    fclose(fid);
-    disp(['Comparison Report saved to ' filename]);
 end
